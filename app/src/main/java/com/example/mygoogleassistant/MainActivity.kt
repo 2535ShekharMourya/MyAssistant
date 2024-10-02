92cfb5dc72279549e88623496930d263216086b5
2535ShekharMourya <113845874+2535ShekharMourya@users.noreply.github.com>
2024-10-02 11:39:57 +0530

    adding top news feature through retrofit and Coroutine

--- app/src/main/java/com/example/mygoogleassistant/MainActivity.kt
+++ app/src/main/java/com/example/mygoogleassistant/MainActivity.kt
@@ -5,19 +5,31 @@ import android.content.pm.PackageManager
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
+import com.example.mygoogleassistant.Networking.RetrofitClient
 import com.example.mygoogleassistant.utils.Utils
 import com.example.mygoogleassistant.utils.Utils.setCustomActionBar
 import com.example.mygoogleassistant.R
 import com.example.mygoogleassistant.assistant.AssistantActivity
 import com.example.mygoogleassistant.assistant.ExploreActivity
+import com.example.mygoogleassistant.assistant.NewsAdapter
+import com.example.mygoogleassistant.databinding.ActivityMainBinding
 import com.example.mygoogleassistant.functions.GoogleLensActivity
+import com.example.mygoogleassistant.uiscreens.NewsActivity
+//import com.example.mygoogleassistant.topnews.NewsActivity
 import com.example.mygoogleassistant.utils.Utils.setCustomActionBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     // Activity and Fragment known as UI Controller
@@ -27,15 +39,27 @@ class MainActivity : AppCompatActivity() {
      Application refers to the whole app and both extend the Context class.*/
 
     // variable for views
+    lateinit var binding: ActivityMainBinding
     private lateinit var imageView: ImageView
     private lateinit var hiGoogle: ImageView
     private lateinit var googleLens: ImageView
     private lateinit var explore: ImageView
+    lateinit var adapter: NewsAdapter
     private val Record_Audio_Request_Code:Int=1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setCustomActionBar(supportActionBar, this)
+
+        makeApiRequest()
+
+       // val topN=findViewById<TextView>(R.id.news_top)
+       /* topN.setOnClickListener {
+            startActivity(Intent(this,NewsActivity::class.java))
+        }*/
         val getCurrentLocation=findViewById<FloatingActionButton>(R.id.get_location)
         // id's of views from xml
         imageView = findViewById(R.id.action_button)
@@ -60,6 +84,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, AssistantActivity::class.java))
             Animatoo.animateDiagonal(this)
         }
+        binding.AllNews.setOnClickListener {
+            startActivity(Intent(this,NewsActivity::class.java))
+        }
         hiGoogle.setOnClickListener {
             startActivity(Intent(this, AssistantActivity::class.java))
             Animatoo.animateSplit(this)
@@ -72,6 +99,34 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, ExploreActivity::class.java))
             Animatoo.animateFade(this)
         }
+    }
+
+    private fun makeApiRequest() {
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.d("NewsResponse","Start Coroutine scope")
+            try {
+                val response = RetrofitClient.instance.getTopHeadlines()
+                Log.d("NewsResponse","New Response getting")
+                if (response.isSuccessful) {
+                    val newsResponse = response.body()
+                    Log.d("NewsResponse","News Article fetch successfully")
+                    withContext(Dispatchers.Main) {
+                        val listOfArticle=newsResponse?.articles
+                        adapter = NewsAdapter(listOfArticle,this@MainActivity)
+                        binding.newsRecyclerview.adapter = adapter
+                        binding.newsRecyclerview.layoutManager =
+                            LinearLayoutManager(this@MainActivity)
+
+                    }
+                } else {
+                    Log.d("NewsResponse", "failed to fetch News Articles")
+                }
+            }catch (e:Exception){
+                Log.d("NewsResponse", "failed to fetch News Articles")
+                e.printStackTrace()
+            }
+        }
+
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
